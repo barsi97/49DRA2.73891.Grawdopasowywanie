bf746a09199afa9c8573810390dd73681bcf78bc
barsi97 <bartosz.myszkiewicz97@gmail.com>
2021-01-12 16:11:42 +0100

    Dodaj pliki projektów.

---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grawdopasowywanie
+{
+    public partial class Form1 : Form
+    {
+        bool allowClick = false;
+        PictureBox firstGuess;
+        Random rnd = new Random();
+        Timer clickTimer = new Timer();
+        int time = 90;
+        Timer timer = new Timer { Interval = 1000 };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private PictureBox[] pictureBoxes
+        {
+            get { return Controls.OfType<PictureBox>().ToArray(); }
+        }
+
+        private static IEnumerable<Image> images
+        {
+            get
+            {
+                return new Image[]
+                {
+                    Properties.Resources.img1,
+                    Properties.Resources.img2,
+                    Properties.Resources.img3,
+                    Properties.Resources.img4,
+                    Properties.Resources.img5,
+                    Properties.Resources.img6,
+                    Properties.Resources.img7,
+                    Properties.Resources.img8,
+                };
+            }
+        }
+
+        private void startGameTimer()
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Koniec czasu!");
+                    ResetImages();
+                }
+
+                var ssTime = TimeSpan.FromSeconds(time);
+
+                label1.Text = "00:" + time.ToString();
+
+            };
+        }
+
+        private void ResetImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Tag = null;
+                pic.Visible = true;
+            }
+            HideImages();
+            setRandomImages();
+            time = 60;
+            timer.Start();
+        }
+
+        private void HideImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Image = Properties.Resources.question;
+            }
+        }
+
+        private PictureBox getFreeSlot()
+        {
+            int num;
+
+            do
+            {
+                num = rnd.Next(0, pictureBoxes.Count());
+            }
+            while (pictureBoxes[num].Tag != null);
+            return pictureBoxes[num];
+        }
+
+        private void setRandomImages()
+        {
+            foreach (var image in images)
+            {
+                getFreeSlot().Tag = image;
+                getFreeSlot().Tag = image;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRandomImages();
+            HideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += label1_Click;
+            button1.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            HideImages();
+
+            allowClick = true;
+            clickTimer.Stop();
+
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (!allowClick) return;
+           var pic = (PictureBox)sender;
+
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+
+            pic.Image = (Image)pic.Tag;
+
+            if (pic.Image == firstGuess.Image && pic != firstGuess)
+
+            {
+                pic.Visible = firstGuess.Visible = false;
+                {
+                    firstGuess = pic;
+                }
+                HideImages();
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+            firstGuess = null;
+            if (pictureBoxes.Any(predicate => predicate.Visible)) return;
+            MessageBox.Show("Wygrałeś! Spróbuj ponownie.");
+            ResetImages();
+        }
+    }
+}
